910e87913925af4559d46f4a880876377f281508
Sambijhouwer <35336214+Sambijhouwer@users.noreply.github.com>
2021-12-02 16:27:59 +0100

    Update main.py

--- day2/main.py
+++ day2/main.py
@@ -1,10 +1,12 @@
 def main():
     with open("data.txt", "r") as file:
         data = file.readlines()
+        
     # Part 1 & Part 2
     x = y = aim = 0
-    data = [x.strip().split() for x in data]
-    for command, number in data:
+    
+    for d in data:
+        command, number = d.split()
         number = int(number)
         if command == "forward":
             x += number
